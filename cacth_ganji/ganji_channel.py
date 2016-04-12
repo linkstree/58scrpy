1ab7a82ec4600a200b211e546871bdcfa04701de
herostree@163.com <herostree@163.com>
2016-04-12 22:38:54 +0800

    add ganji cactch

---
+++ cacth_ganji/ganji_channel.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pymongo
+
+wb_text = requests.get('http://bj.ganji.com/wu/')
+wb_text.encoding='utf-8'
+soup=BeautifulSoup(wb_text.text,'lxml')
+def get_all_channel():
+    allcateory_urls = list(map(lambda x:x.get('href'),soup.select('dl.fenlei dt a')))
+    all_channel=list(map(lambda x:x.strip('/'),allcateory_urls))
+    return all_channel
+
---
+++ cacth_ganji/get_item_info.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pymongo
+import ganji_channel
+
+list_view=
