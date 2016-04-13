dc50402dd58078010bf9071a5f3ca94d897a0d4c
herostree@163.com <herostree@163.com>
2016-04-13 22:52:26 +0800

    add ganji cactch

--- cacth_ganji/get_item_info.py
+++ cacth_ganji/get_item_info.py
@@ -6,37 +6,32 @@ import pymongo
 client = pymongo.MongoClient('127.0.0.1',port=27017)
 cacth_ganji=client['cacth_ganji']
 channel_urls = cacth_ganji['channel_urls']
+item_urls = cacth_ganji['item_urls']
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
         'referer':'http://bj.ganji.com/wu/'}
+
 def get_item_url(channel,page,who_sell=0):
     for i in channel:
         for p in page:
             channel_url='http://bj.ganji.com/{}/o{}'.format(i,p)
+            wb_text=requests.get(channel_url)
+            soup = BeautifulSoup(wb_text.text,'lxml')
+            if len(soup.select('li.js-item a.ft-tit')) == 5:
+                break
             if not channel_urls.find_one({'channel_url':channel_url,'crawled':'false'}):
                 channel_urls.insert({'channel_url':channel_url,'crawled':'false'})
 
 def url_spider():
-    # print(len(list(channel_urls.find({'crawled':'false'}))))
     for i in channel_urls.find({'crawled':'false'}):
         wb_text=requests.get(i['channel_url'],headers=headers)
         soup=BeautifulSoup(wb_text.text,'lxml')
-        # urls=soup.select('ul li.js-item a.ft-tit')
-        # urls=[b.get('href') for b in soup.select('ul li.js-item a.ft-tit')]
-        for b in soup.select('ul li.js-item a.ft-tit'):
-            print(b)
-            print(len(soup.select('ul li.js-item a.ft-tit')))
-        # print(urls)
-get_item_url(ganji_channel.get_all_channel(),range(1,4))
+        urls=[b.get('href') for b in soup.select('ul li.js-item a.ft-tit') ]
+        for a in urls:
+            if 'click' in a and not item_urls.find_one({'item_url':a}):
+                wb_te=requests.get(a)
+                item_urls.insert({'item_url':wb_te.url})
+            elif not item_urls.find_one({'item_url':a}):
+                item_urls.insert({'item_url':a})
+
 url_spider()
-print(list(channel_urls.find({'crawled':'false'})))
-
-# wb_text=requests.get('http://bj.ganji.com/jiaju/o1/',headers=headers)
-# soup = BeautifulSoup(wb_text.text,'lxml')
-# url=soup.select('ul li.js-item a.ft-tit')
-# a=0
-# for i in url:
-#     a+=1
-#     print(a,i.get_text())
-# print(len(url))
-
 
