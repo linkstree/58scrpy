72c3acd3dd78d97fd0a299411259770aaa885772
herostree@163.com <herostree@163.com>
2016-04-14 22:21:15 +0800

    add ganji cactch

--- cacth_ganji/get_item_info.py
+++ cacth_ganji/get_item_info.py
@@ -20,15 +20,7 @@ def get_item_url(channel,page,who_sell=0):
                 continue
             wb_text=requests.get(channel_url)
             soup = BeautifulSoup(wb_text.text,'lxml')
-            # if len(soup.select('li.js-item a.ft-tit')) == 5 and '抱歉' in soup.select('div.no-search dl dd ')[0].text:
-            '''if not soup.select('.next') or len(soup.select('ul li.js-item a.ft-tit')) == 5:
-                print('由于下一页按钮没有或者该页面只select到5个条目,所以认为该页不存在',channel_url)
-                break
-            if not channel_urls.find_one({'channel_url':channel_url,'crawled':'false'}) and 'sorry' not in channel_url:
-                print('哈哈,已经将该页插入到channel_urls库中了，可以被url_spider搞了',channel_url)
-                channel_urls.insert({'channel_url':channel_url,'crawled':'false'})
-                '''
-            if soup.select('.next') or len(soup.select('ul li.js-item a.ft-tit')) != 5:
+            if soup.select('.next') and len(soup.select('ul li.js-item a.ft-tit')) != 5:
                 channel_urls.insert({'channel_url': channel_url, 'crawled': 'false'})
                 print('哈哈,已经将该页插入到channel_urls库中了，可以被url_spider搞了',channel_url)
             else:
@@ -36,24 +28,43 @@ def get_item_url(channel,page,who_sell=0):
                 break
             time.sleep(2)
 
+def get_others_commodity_url_to_db(channel_url):
+    #这三个类目下的商品页面属性不一样，杂七杂八 免费赠送 物品交换
+    wb_text = requests.get(channel_url,headers=headers)
+    soup = BeautifulSoup(wb_text.text,'lxml')
+    urls=[b.get('href') for b in soup.select('a.infor-title01.com-title')]
+    for a in urls:
+        if 'click' in a and not item_urls.find_one({'item_url':a}):
+            wb_text=requests.get(a,headers=headers)
+            item_urls.insert({'item_url':wb_text.url})
+            print('哈哈，已经将商品的详情页的url加入到item_urls库了')
+        elif not item_urls.find_one({'item_url':a}):
+            item_urls.insert({'item_url':a})
+            print('哈哈，已经将商品的详情页的url加入到item_urls库了')
+        channel_urls.update({'_id':a['_id']},{'$set':{'crawled':'true'}})
+        time.sleep(2)
 def url_spider():
     for i in channel_urls.find({'crawled':'false'}):
-        wb_text=requests.get(i['channel_url'],headers=headers)
-        soup=BeautifulSoup(wb_text.text,'lxml')
-        urls=[b.get('href') for b in soup.select('ul li.js-item a.ft-tit') ]
-        for a in urls:
-            if 'click' in a and not item_urls.find_one({'item_url':a}):
-                wb_te=requests.get(a)
-                item_urls.insert({'item_url':wb_te.url})
-                print('哈哈，已经设定目标',wb_te.url)
-            elif not item_urls.find_one({'item_url':a}):
-                item_urls.insert({'item_url':a})
-                print('哈哈，已经设定目标',wb_te.url)
-            channel_urls.update({'_id':a['_id']},{'$set':{'crawled':'true'}})
-            time.sleep(2)
+        if 'qitawupin' in i or 'ershoufree' in i or 'wupinjiaohuan' in i :
+            get_others_commodity_url_to_db(i)
+        else:
+            wb_text=requests.get(i['channel_url'],headers=headers)
+            soup=BeautifulSoup(wb_text.text,'lxml')
+            urls=[b.get('href') for b in soup.select('ul li.js-item a.ft-tit') ]
+            for a in urls:
+                if 'click' in a and not item_urls.find_one({'item_url':a}):
+                    wb_te=requests.get(a)
+                    item_urls.insert({'item_url':wb_te.url})
+                    print('哈哈，已经设定目标',wb_te.url)
+                elif not item_urls.find_one({'item_url':a}):
+                    item_urls.insert({'item_url':a})
+                    print('哈哈，已经设定目标',wb_te.url)
+                channel_urls.update({'_id':a['_id']},{'$set':{'crawled':'true'}})
+                time.sleep(2)
 
 get_item_url(ganji_channel.get_all_channel(),range(1,1000))
 url_spider()
+# get_others_commodity_url_to_db('http://bj.ganji.com/wupinjiaohuan/o2')
 
 
 
