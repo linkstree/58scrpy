4a27957c74732be4fbb77f1a0741d751d59cfbf4
herostree@163.com <tx624624>
2016-04-21 15:13:45 +0800

    Merge branch 'master' of https://github.com/linkstree/58scrpy
    # Conflicts:
    #	cacth_ganji/get_item_info.py

--- cacth_ganji/ganji_channel.py
+++ cacth_ganji/ganji_channel.py
@@ -9,5 +9,9 @@ soup=BeautifulSoup(wb_text.text,'lxml')
 def get_all_channel():
     allcateory_urls = list(map(lambda x:x.get('href'),soup.select('dl.fenlei dt a')))
     all_channel=list(map(lambda x:x.strip('/'),allcateory_urls))
-    return all_channel
+    channel=[]
+    for i in all_channel:
+        if 'shoujihaoma' not in i:
+            channel.append(i)
+    return channel
 
